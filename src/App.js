2c1f4cc1cb31244afe3315f54284167fd1948667
273* <keita.y310@icloud.com>
2025-02-16 18:56:35 +0900

    3d表示の設定

--- src/App.js
+++ src/App.js
@@ -4,10 +4,17 @@
 import { Scene } from "./DEMO";
 import Test1 from "./Test1";
 import Test2 from "./Test2";
+import Test3 from "./Test3";
 
 export const App = () => (
   <div className="container">
-    <div className="nav">
+    <div className="header">
+      <p>Hello</p>
+      <p>273*</p>
+      <p>Portfolio2025</p>
+    </div>
+
+    {/* <div className="nav">
       <h1 className="label" />
       <div />
       <span className="caption" />
@@ -16,9 +23,10 @@ export const App = () => (
         <div className="button">VISIT LUSION</div>
       </a>
       <div className="button gray">///</div>
-    </div>
+    </div> */}
     {/* <Scene style={{ borderRadius: 20 }} /> */}
     {/* <Test1 /> */}
-    <Test2 />
+    {/* <Test2 /> */}
+    <Test3 />
   </div>
 );
--- src/Test3.js
+++ src/Test3.js
@@ -6,7 +6,6 @@ import {
   MeshTransmissionMaterial,
   Environment,
   Lightformer,
-  Circle,
 } from "@react-three/drei";
 import {
   CuboidCollider,
@@ -17,25 +16,22 @@ import {
 } from "@react-three/rapier";
 import { EffectComposer, N8AO } from "@react-three/postprocessing";
 import { easing } from "maath";
-import { func } from "three/examples/jsm/nodes/Nodes.js";
-import { useLoader } from "@react-three/fiber";
-import { SVGLoader } from "three/examples/jsm/loaders/SVGLoader";
 
 const accents = ["#4060ff", "#20ffa0", "#ff4060", "#ffcc00"];
 const shuffle = (accent = 0) => [
   { color: "#444", roughness: 0.1 },
   { color: "#444", roughness: 0.75 },
-  { color: "#444", roughness: 0.75 },
-  //   { color: "white", roughness: 0.1 },
+  //   { color: "#444", roughness: 0.75 },
+  { color: "white", roughness: 0.1 },
   //   { color: "white", roughness: 0.75 },
-  //   { color: "white", roughness: 0.1 },
-  //   { color: accents[accent], roughness: 0.1, accent: true },
-  //   { color: accents[accent], roughness: 0.75, accent: true },
-  //   { color: accents[accent], roughness: 0.1, accent: true },
+  { color: "white", roughness: 0.1 },
+  { color: accents[accent], roughness: 0.1, accent: true },
+  { color: accents[accent], roughness: 0.75, accent: true },
+  { color: accents[accent], roughness: 0.1, accent: true },
 ];
 
 // 3Dシーンのコンポーネント
-export default function Test2(props) {
+export default function Test3(props) {
   const [accent, click] = useReducer((state) => ++state % accents.length, 0);
   // 各オブジェクトの色や質感をランダム化
   const connectors = useMemo(() => shuffle(accent), [accent]);
@@ -46,11 +42,12 @@ export default function Test2(props) {
       shadows // シャドウを有効化
       dpr={[1, 1.5]} // デバイスピクセル比を設定
       gl={{ antialias: false }} // アンチエイリアスを無効化
-      camera={{ position: [0, 0, 15], fov: 17.5, near: 1, far: 20 }}
+      camera={{ position: [0, 14, 0], fov: 17.5, near: 1, far: 20 }}
       {...props}
+      className="threeD"
     >
       {/* 初期背景 */}
-      <color attach="background" args={["#fff"]} />
+      <color attach="background" args={["#111"]} />
 
       {/* ライトの設定 */}
       <ambientLight intensity={0.4} />
@@ -63,9 +60,10 @@ export default function Test2(props) {
       />
 
       {/* Physicsで物理エンジンを適用 */}
-      <Physics /*debug*/ gravity={[0, 0, 0]}>
+      <Physics /*debug*/ gravity={[0, 0, 0]} debug={true}>
         {/* ポインター */}
         <Pointer />
+
         {/* Connectorコンポーネントを複数生成 */}
         {
           connectors.map((props, i) => <Connector key={i} {...props} />) /* prettier-ignore */
@@ -74,7 +72,7 @@ export default function Test2(props) {
           <Model>
             {/* ガラス風の光の透過マテリアルを設定 */}
             <MeshTransmissionMaterial
-              clearcoat={1} // クリアコート（反射の強さ），１で最大（ガラスのような光沢）
+              clearcoat={2} // クリアコート（反射の強さ），１で最大（ガラスのような光沢）
               thickness={0.1} // 厚みの影響度（ガラスの厚み）
               anisotropicBlur={0.1} // 異方性ぼかし（透明部分のぼやけ具合）
               chromaticAberration={0.1} // 色収差（光の屈折による虹色のにじみ）
@@ -83,7 +81,32 @@ export default function Test2(props) {
             />
           </Model>
         </Connector>
-        <SVGModel url="/github.svg" />
+        <Connector position={[10, 10, 5]}>
+          <Model>
+            {/* ガラス風の光の透過マテリアルを設定 */}
+            <MeshTransmissionMaterial
+              clearcoat={2} // クリアコート（反射の強さ），１で最大（ガラスのような光沢）
+              thickness={0.1} // 厚みの影響度（ガラスの厚み）
+              anisotropicBlur={0.1} // 異方性ぼかし（透明部分のぼやけ具合）
+              chromaticAberration={0.1} // 色収差（光の屈折による虹色のにじみ）
+              samples={8} // レンダリング時のサンプリング数（高いほど滑らか）
+              resolution={512} // 屈折のテクスチャ解像度（高いほど詳細だが重い）
+            />
+          </Model>
+        </Connector>
+        <Connector position={[10, 10, 5]}>
+          <Model>
+            {/* ガラス風の光の透過マテリアルを設定 */}
+            <MeshTransmissionMaterial
+              clearcoat={2} // クリアコート（反射の強さ），１で最大（ガラスのような光沢）
+              thickness={0.1} // 厚みの影響度（ガラスの厚み）
+              anisotropicBlur={0.1} // 異方性ぼかし（透明部分のぼやけ具合）
+              chromaticAberration={0.1} // 色収差（光の屈折による虹色のにじみ）
+              samples={8} // レンダリング時のサンプリング数（高いほど滑らか）
+              resolution={512} // 屈折のテクスチャ解像度（高いほど詳細だが重い）
+            />
+          </Model>
+        </Connector>
       </Physics>
 
       {/* EffectComposerでポストプロセスエフェクト(ポストプロセス処理)を適用 */}
@@ -146,13 +169,23 @@ function Connector({
   // フレームごとに物理演算を適用
   useFrame((state, delta) => {
     delta = Math.min(0.1, delta);
+
+    const centerOffset = new THREE.Vector3(1.8, 0, 0); // X方向に +1 ずらす
+
     // オブジェクトを中心に引っ張る力を加えている
     // applyImpulse を使って，オブジェクトを中央に向かうように動かす
     // negate().multiplyScalar(0.2) によって，現在の位置の逆方向に力を加える
     api.current?.applyImpulse(
-      vec.copy(api.current.translation()).negate().multiplyScalar(0.2)
+      vec
+        .copy(api.current.translation())
+        .sub(centerOffset)
+        .negate()
+        .multiplyScalar(0.2)
     );
   });
+
+  console.log(api);
+  const v = 2;
   // RigidBodyで物理演算を適用
   return (
     <RigidBody
@@ -160,14 +193,19 @@ function Connector({
       angularDamping={1} // 角速度ダンピング
       friction={0.1} // 摩擦
       position={pos} // 位置
+      rotation={pos}
       ref={api}
       colliders={false} // コライダーを無効化
     >
       {/* 3 つの CuboidCollider を使ってオブジェクトの衝突判定を設定 */}
       {/* 3つのパイプだからか */}
-      <CuboidCollider args={[0.38, 1.27, 0.38]} />
-      <CuboidCollider args={[1.27, 0.38, 0.38]} />
-      <CuboidCollider args={[0.38, 0.38, 1.27]} />
+      {/* <CuboidCollider args={[0.38, 1.27, 0.38]} />
+      <CuboidCollider args={[1.27, 0.38, 0.38]} /> */}
+      <CuboidCollider
+        args={[0.8 * v, 0.1, 0.3 * v]}
+        position={[0.7 * v, 0.1, -0.4 * v]}
+      />
+      {/* <TrimeshCollider args={[api.選択範囲.geometry]} /> */}
       {children ? children : <Model {...props} />}
       {accent && (
         // アクセント色のポイントライト(distanceで影響範囲を指定)
@@ -198,7 +236,7 @@ function Pointer({ vec = new THREE.Vector3() }) {
       ref={ref}
     >
       {/* マウスカーソルの形状は球体 */}
-      <BallCollider args={[1]} />
+      <BallCollider args={[0.75]} />
     </RigidBody>
   );
 }
@@ -207,7 +245,13 @@ function Pointer({ vec = new THREE.Vector3() }) {
 function Model({ children, color = "white", roughness = 0, ...props }) {
   const ref = useRef();
   // glTFファイルを読み込む
-  const { nodes, materials } = useGLTF("/c-transformed.glb");
+  // const { nodes, materials } = useGLTF("/c-transformed.glb");
+  const { nodes, materials } = useGLTF("/273do_logo.glb");
+
+  // 読み込んだGLBのデータを確認
+  console.log("Nodes:", nodes);
+  console.log("Materials:", materials);
+
   useFrame((state, delta) => {
     // easing.dampC で色を滑らかに変化させる
     easing.dampC(ref.current.material.color, color, 0.2, delta);
@@ -217,85 +261,18 @@ function Model({ children, color = "white", roughness = 0, ...props }) {
       ref={ref}
       castShadow
       receiveShadow
-      scale={10}
-      geometry={nodes.connector.geometry}
+      scale={20}
+      geometry={nodes.mesh.geometry}
+      //   rotation={[90, 0, 0]}
     >
+      {/* <TrimeshCollider args={[nodes.mesh.geometry]} /> */}
       {/* マテリアル設定 */}
       <meshStandardMaterial
         metalness={0.2}
         roughness={roughness}
-        map={materials.base.map}
+        // map={materials.base.map}
       />
       {children}
     </mesh>
-  );
-}
-
-// SVGの3Dモデルを読み込む
-function SVGModel({
-  r = THREE.MathUtils.randFloatSpread,
-  vec = new THREE.Vector3(),
-  url = "/github.svg",
-}) {
-  // SVGLoader を使ってデータを読み込む
-  const svgData = useLoader(SVGLoader, url);
-
-  const pos = useMemo(() => [r(2), r(2), r(2)], []);
-  //   const ref = useRef(); // RigidBody の参照
-  //   useFrame((state, delta) => {
-  //     delta = Math.min(0.1, delta);
-  //     ref.current?.applyImpulse(
-  //       vec
-  //         .copy(ref.current.translation())
-  //         .negate()
-  //         .multiplyScalar(0.02 * delta)
-  //     );
-  //   });
-  return (
-    <RigidBody
-      //   ref={ref}
-      type="dynamic"
-      mass={1}
-      colliders={false}
-      linearDamping={4} // 線形ダンピング
-      angularDamping={1} // 角速度ダンピング
-      friction={0.1} // 摩擦
-      position={pos} // 位置
-    >
-      {/* 円形の当たり判定 */}
-      {/* <BallCollider args={[1]} /> */}
-      <CuboidCollider args={[1, 1, 0]} />
-
-      <group position={[0, 0, 0]} scale={0.1} rotation={[Math.PI, 0, 0]}>
-        {svgData.paths.map((path, i) => {
-          const shapes = path.toShapes(true);
-
-          return shapes.map((shape, j) => {
-            return (
-              <mesh key={`${i}-${j}`}>
-                <extrudeGeometry
-                  attach="geometry"
-                  args={[shape, { depth: 4, bevelEnabled: true, steps: 10 }]}
-                />
-                <MeshTransmissionMaterial
-                  //   clearcoat={1}
-                  //   thickness={10.1}
-                  //   anisotropicBlur={10.1}
-                  //   chromaticAberration={0.1}
-                  //   samples={8}
-                  //   resolution={512}
-                  clearcoat={1}
-                  thickness={5.1}
-                  anisotropicBlur={8.1}
-                  chromaticAberration={0.1}
-                  samples={8}
-                  //   resolution={1024}
-                />
-              </mesh>
-            );
-          });
-        })}
-      </group>
-    </RigidBody>
   );
 }
